1d0f34c1414c0e96ce6f8d1a5f2053d77ae560af
Manuel Egger <egger.m95@gmail.com>
2018-03-07 13:04:53 +0100

    added working GUI

--- app/src/main/java/com/manuelegger/palindromechecker/MainActivity.java
+++ app/src/main/java/com/manuelegger/palindromechecker/MainActivity.java
@@ -2,6 +2,11 @@ package com.manuelegger.palindromechecker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button checkPalindrome = (Button) findViewById(R.id.buttonCheckPalindrome);
+        EditText input = (EditText) findViewById(R.id.editTextInput);
+        final TextView output = (TextView) findViewById(R.id.textViewOutput);
+
+        checkPalindrome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                output.setText("Button klicked");
+            }
+        });
+
     }
 }
